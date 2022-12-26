331afa33b0f3af8ec4ac82b7f14a0214f80d8e45
Zachary Fuller <zachary.fuller.dev@gmail.com>
2022-12-25 23:54:30 -0500

    Tests setup

---
+++ src/day-1.spec.ts
@@ -0,0 +1,9 @@
+import {assert} from "chai";
+import {day1} from "./day-1";
+
+describe("part one", () => {
+  it("returns an empty string (example)", () => {
+    const result = day1.solveForPartOne("");
+    assert.equal(result, "");
+  });
+});
--- src/day-1.ts
+++ src/day-1.ts
@@ -3,7 +3,7 @@ import {Day} from "./day";
 export const day1: Day = {
   solveForPartOne(input: string) {
     let inArray: string[] = input.toString().split("\n");
-    return inArray[0];
+    return "";
   },
   solveForPartTwo(input: string) {
     return "";
