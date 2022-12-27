0d09f66f3899e2f376aa013b5e2ee49c0691679e
Zachary Fuller <zachary.fuller.dev@gmail.com>
2022-12-26 19:57:48 -0500

    Complete day 1, part 1 & part 2

--- src/day-1.spec.ts
+++ src/day-1.spec.ts
@@ -1,9 +1,21 @@
 import {assert} from "chai";
 import {day1} from "./day-1";
+import fs from "fs";
 
 describe("part one", () => {
-  it("returns an empty string (example)", () => {
-    const result = day1.solveForPartOne("");
-    assert.equal(result, "");
+  it("Gets max calorie from sample", () => {
+    const input = fs.readFileSync("./inputs/sample-input-day-1.txt" , "utf8")
+
+    const result = day1.solveForPartOne(input);
+    assert.equal(result, "24000");
+  });
+});
+
+describe("part two", () => {
+  it("Gets correct calorie sum from sample", () => {
+    const input = fs.readFileSync("./inputs/sample-input-day-1.txt" , "utf8")
+
+    const result = day1.solveForPartTwo(input);
+    assert.equal(result, "45000");
   });
 });
--- src/day-1.ts
+++ src/day-1.ts
@@ -2,10 +2,36 @@ import {Day} from "./day";
 
 export const day1: Day = {
   solveForPartOne(input: string) {
-    let inArray: string[] = input.toString().split("\n");
-    return "";
+    const inArray: string[] = input.toString().split("\n");
+    let maxCalorie = 0;
+    let calorieSum: number = 0;
+
+    inArray.forEach((item) => {
+      if (item === "") {
+        if (calorieSum > maxCalorie) maxCalorie = calorieSum;
+        calorieSum = 0;
+      }
+      calorieSum += Number(item);
+    });
+
+    return String(maxCalorie);
   },
+
   solveForPartTwo(input: string) {
-    return "";
+    const inArray: string[] = input.toString().split("\n");
+    let summedCalories: number[] = [];
+    let calorieSum: number = 0;
+
+    inArray.forEach((item) => {
+      if (item === "") {
+        summedCalories.push(calorieSum);
+        calorieSum = 0;
+      }
+      calorieSum += Number(item);
+    });
+
+    summedCalories.sort((a, b) => b - a);
+
+    return String(summedCalories[0] + summedCalories[1] + summedCalories[2]);
   }
 }
